7dc2e12be80167ed2343967fca8d29503584018d
rafacostavilla <rafaelcostavilla@gmail.com>
2024-10-03 18:05:39 +1300

    Create a Lisp-like cons list using Box

---
+++ src/main.rs
@@ -0,0 +1,6 @@
+use rust_smartpointers_box_cons_sample::List::{Cons, Nil};
+
+fn main() {
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    println!("{:?}", list);
+}
